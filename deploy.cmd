eaccc755727613c9a6aca485fc6f81640fcd28a5
Maurycy Markowski <maumar@microsoft.com>
2016-05-09 16:10:01 -0700

    costam costam

--- deploy.cmd
+++ deploy.cmd
@@ -12,20 +12,20 @@ setlocal enabledelayedexpansion
 echo Handling .NET Web Application deployment.
 
 :: We don't need the XML docs
-::set NUGET_XMLDOC_MODE=skip
+set NUGET_XMLDOC_MODE=skip
 
 :: Make the NuGet packages go in a persisted folder instead of %USERPROFILE%, which on Azure goes in Temp space
-::set NUGET_PACKAGES=%HOME%\.nuget
+set NUGET_PACKAGES=%HOME%\nuget
 
 :: For now, we need a myget feed since RC2 packages are not yet released
 :: call :ExecuteCmd .nuget\nuget.exe restore -source https://myget.org/f/aspnetrelease/api/v3/index.json -source https://api.nuget.org/v3/index.json -packagesavemode nuspec
 
 call :ExecuteCmd %HOME%\dotnet\dotnet.exe restore -s https://myget.org/f/aspnetrelease/api/v3/index.json -s https://api.nuget.org/v3/index.json
-call :ExecuteCmd %HOME%\dotnet\dotnet.exe publish
+
 IF !ERRORLEVEL! NEQ 0 goto error
 
+call :ExecuteCmd %HOME%\dotnet\dotnet.exe publish --output "%DEPLOYMENT_TEMP%"
 ::call :ExecuteCmd dotnet publish --output "%DEPLOYMENT_TEMP%"
-IF !ERRORLEVEL! NEQ 0 goto error
 
 ::call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TEMP%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
 IF !ERRORLEVEL! NEQ 0 goto error
